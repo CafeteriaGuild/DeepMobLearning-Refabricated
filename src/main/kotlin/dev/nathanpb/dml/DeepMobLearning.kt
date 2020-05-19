43ddef060d81e58a0ae7533a8f4e0b2e6f0b4b2d
NathanPB <nathan.pbombana@gmail.com>
2020-05-19 06:42:39 -0300

    Licensed the source code over GPL-v3

--- src/main/kotlin/dev/nathanpb/dml/DeepMobLearning.kt
+++ src/main/kotlin/dev/nathanpb/dml/DeepMobLearning.kt
@@ -3,6 +3,14 @@ package dev.nathanpb.dml
 import dev.nathanpb.dml.item.registerItems
 import net.minecraft.util.Identifier
 
+/*
+ * Copyright (C) 2020 Nathan P. Bombana, IterationFunk
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
 @Suppress("unused")
 fun init() {
     registerItems()
