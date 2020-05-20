c470e0019693be2ff5159036c15ac6fc5727778b
NathanPB <nathan.pbombana@gmail.com>
2020-05-19 23:50:16 -0300

    Rendered the Deep Learner GUI

--- src/main/kotlin/dev/nathanpb/dml/gui/GuiDeeplearner.kt
+++ src/main/kotlin/dev/nathanpb/dml/gui/GuiDeeplearner.kt
@@ -1,8 +1,13 @@
 package dev.nathanpb.dml.gui
 
+import com.mojang.blaze3d.platform.GlStateManager
 import dev.nathanpb.dml.container.ContainerDeepLearner
+import dev.nathanpb.dml.identifier
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.ingame.ContainerScreen
+import net.minecraft.text.LiteralText
 import net.minecraft.text.TranslatableText
+import net.minecraft.util.Identifier
 
 /*
  * Copyright (C) 2020 Nathan P. Bombana, IterationFunk
@@ -17,9 +22,22 @@ class GuiDeeplearner (
 ) : ContainerScreen<ContainerDeepLearner>(
     container,
     container.playerInventory,
-    TranslatableText("deep_learner")
+    TranslatableText("item.deepmoblearning.deep_learner")
 ) {
-    override fun drawBackground(delta: Float, mouseX: Int, mouseY: Int) {
 
+    override fun render(mouseX: Int, mouseY: Int, delta: Float) {
+        this.renderBackground()
+        super.render(mouseX, mouseY, delta)
+        this.drawMouseoverTooltip(mouseX, mouseY)
+    }
+
+    override fun drawBackground(delta: Float, mouseX: Int, mouseY: Int) {
+        GlStateManager.color4f(1.0f, 1.0f, 1.0f, 1.0f)
+        minecraft?.textureManager?.bindTexture(identifier("textures/gui/deeplearner_base.png"))
+        blit((this.width - this.containerWidth) / 2, (this.height - this.containerHeight) / 2, 0, 0, containerWidth, containerHeight)
+    }
+
+    override fun drawForeground(mouseX: Int, mouseY: Int) {
+        super.font.draw(title.asFormattedString(), 8F, 6F, 0x40A0D3)
     }
 }
