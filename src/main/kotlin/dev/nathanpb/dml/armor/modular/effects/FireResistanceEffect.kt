3211e496a9c01091cf418e33c60c05977f8d953d
NathanPB <nathan.pbombana@gmail.com>
2020-08-16 21:21:01 -0300

    Fire Resistance effect working

--- src/main/kotlin/dev/nathanpb/dml/armor/modular/effects/FireResistanceEffect.kt
+++ src/main/kotlin/dev/nathanpb/dml/armor/modular/effects/FireResistanceEffect.kt
@@ -19,17 +19,19 @@
 
 package dev.nathanpb.dml.armor.modular.effects
 
-import dev.nathanpb.dml.armor.modular.core.ModularEffect
+import dev.nathanpb.dml.armor.modular.ProtectionLikeEffect
 import dev.nathanpb.dml.armor.modular.core.ModularEffectContext
 import dev.nathanpb.dml.config
 import dev.nathanpb.dml.data.ModularArmorData
 import dev.nathanpb.dml.enums.DataModelTier
 import dev.nathanpb.dml.enums.EntityCategory
+import dev.nathanpb.dml.event.context.LivingEntityDamageEvent
 import dev.nathanpb.dml.identifier
 import net.minecraft.entity.attribute.EntityAttributeModifier
+import net.minecraft.entity.damage.DamageSource
 import net.minecraft.text.TranslatableText
 
-class FireResistanceEffect : ModularEffect(
+class FireResistanceEffect : ProtectionLikeEffect(
     identifier("fire_immunity"),
     EntityCategory.NETHER,
     config.glitchArmor::enableFireImmunity,
@@ -39,8 +41,18 @@ class FireResistanceEffect : ModularEffect(
     override val name = TranslatableText("effect.minecraft.fire_resistance")
 
     override fun registerEvents() {
-
+        LivingEntityDamageEvent.register { context ->
+            if (protectsAgainst(context.source)) {
+                val protection = getProtectionAmount(context.entity.armorItems.toList())
+                if (protection > 0) {
+                    return@register context.copy(damage = 0F)
+                }
+            }
+            null
+        }
     }
+
+    override fun protectsAgainst(source: DamageSource) = source.isFire
 
     override fun createEntityAttributeModifier(armor: ModularArmorData): EntityAttributeModifier {
         return EntityAttributeModifier(id.toString(), 1.0, EntityAttributeModifier.Operation.MULTIPLY_BASE)
