f16eb61879e059febb4bbbe926f74b01e92ab141
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2022-12-18 17:50:45 -0300

    Divide Simulacrum's entrypoints into separate classes

--- simulacrum/src/main/kotlin/dev/nathanpb/dml/simulacrum/Init.kt
+++ simulacrum/src/main/kotlin/dev/nathanpb/dml/simulacrum/Init.kt
@@ -3,34 +3,22 @@ package dev.nathanpb.dml.simulacrum
 import dev.nathanpb.dml.config
 import dev.nathanpb.dml.identifier
 import dev.nathanpb.dml.item.ITEM_GROUP
-import dev.nathanpb.dml.item.ItemDataModel
 import dev.nathanpb.dml.simulacrum.block.chamber.BlockEntitySimulationChamber
 import dev.nathanpb.dml.simulacrum.block.chamber.BlockSimulationChamber
 import dev.nathanpb.dml.simulacrum.item.registerItems
 import dev.nathanpb.dml.simulacrum.screen.ScreenHandlerSimulationChamber
-import dev.nathanpb.dml.simulacrum.screen.ScreenSimulationChamber
-import dev.nathanpb.dml.simulacrum.util.DataModelUtil
-import dev.nathanpb.dml.utils.RenderUtils.Companion.getTextWithDefaultTextColor
-import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.`object`.builder.v1.block.entity.FabricBlockEntityTypeBuilder
 import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntityType
-import net.minecraft.client.MinecraftClient
-import net.minecraft.client.gui.screen.ingame.HandledScreens
-import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.item.BlockItem
-import net.minecraft.item.ItemStack
 import net.minecraft.network.PacketByteBuf
 import net.minecraft.screen.ScreenHandlerType
-import net.minecraft.text.Text
-import net.minecraft.util.Formatting
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.registry.Registry
-import net.minecraft.world.World
 import team.reborn.energy.api.EnergyStorage
 
 
@@ -77,39 +65,4 @@ fun init() {
         { blockEntity, _ -> blockEntity.energyStorage },
         SIMULATION_CHAMBER_ENTITY
     )
-}
-
-@Suppress("unused")
-fun initClient() {
-    HandledScreens.register(SCS_HANDLER_TYPE) { handler, inventory, title ->
-        ScreenSimulationChamber(handler, inventory, title)
-    }
-
-    ItemTooltipCallback.EVENT.register(ItemTooltipCallback { item: ItemStack, _: TooltipContext?, lines: MutableList<Text?> ->
-        val world: World? = MinecraftClient.getInstance().world
-        if (item.item is ItemDataModel && DataModelUtil.getEntityCategory(item) != null) {
-            world?.let {
-                lines.add(
-                    getTextWithDefaultTextColor(Text.translatable(
-                        "tooltip.dml-refabricated.data_model.1"),
-                        it
-                    )
-                    .append(Text.translatable(
-                        "tooltip.dml-refabricated.data_model.2",
-                        DataModelUtil.getEnergyCost(item)
-                    ).formatted(Formatting.WHITE))
-                )
-                lines.add(
-                    getTextWithDefaultTextColor(
-                        Text.translatable("tooltip.dml-refabricated.data_model.3"),
-                        it
-                    )
-                    .append(Text.translatable(
-                        "tooltip.dml-refabricated.data_model.4",
-                        DataModelUtil.textType(item)
-                    ).formatted(Formatting.WHITE))
-                )
-            }
-        }
-    })
 }
